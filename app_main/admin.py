499923907804d14e6f9775975cebee4f46ec8e1f
MyEternity <spddarkforce47@gmail.com>
2022-11-14 14:57:15 +0300

    admin panel

--- app_main/admin.py
+++ app_main/admin.py
@@ -1,1 +1,7 @@
-# Register your models here.
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Status)
+admin.site.register(StatusFlow)
+admin.site.register(Department)
+admin.site.register(Ware)
