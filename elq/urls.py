5cb8c438eced7d8d34f9ec7d2d2682373dca413c
MyEternity <spddarkforce47@gmail.com>
2022-11-14 16:58:26 +0300

    template rendering

--- elq/urls.py
+++ elq/urls.py
@@ -3,13 +3,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import RedirectView
+from app_main.views import IndexPageView
 
 urlpatterns = [
     path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico', permanent=True)),
     path('admin/', admin.site.urls),
     path('users/', include("app_users.urls")),
     path('devices/', include("app_devices.urls")),
-    path('', include("app_main.urls")),
+    path('', IndexPageView.as_view(), name='index'),
 ]
 
 if settings.DEBUG:
