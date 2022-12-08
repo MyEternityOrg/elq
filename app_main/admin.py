c280662b45b5fb388e3b5e1dc459e7dd770150a1
spddarkforce <mlcpqk8q3drt0o2w@@DVC>
2022-12-08 10:07:13 +0300

    model inlines

--- app_main/admin.py
+++ app_main/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import *
 
+
+class DocumetLine(admin.StackedInline):
+    model = DocumentWare
+    extra = 0
+
+
+class DocWare(admin.ModelAdmin):
+    inlines = [DocumetLine]
+
+
 admin.site.register(Status)
 admin.site.register(StatusFlow)
 admin.site.register(Department)
+admin.site.register(Document, DocWare)
 admin.site.register(Ware)
