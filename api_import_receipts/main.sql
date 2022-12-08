ca3f68e0a6b38c56fde7820b6e40488225b4af51
spddarkforce <mlcpqk8q3drt0o2w@@DVC>
2022-12-08 12:13:36 +0300

    print line len

--- api_import_receipts/main.sql
+++ api_import_receipts/main.sql
@@ -1,23 +1,20 @@
-select
-	cs.cashnum as cash_id,
-	cs.numshift as shift_id,
-	cp.numberfield as check_id,
-	to_char(cs.shiftopen,'YYYY-MM-DD') as check_date,
-	ch.item as ware,
-	sum(case 
-		when ch.precision < 1 then ch.qnty * ch.precision
-		else ch.qnty / 1000
-	end) as ware_amount
+select cs.cashnum                          as cash_id,
+       cs.numshift                         as shift_id,
+       cp.numberfield                      as check_id,
+       to_char(cs.shiftopen, 'YYYY-MM-DD') as check_date,
+       ch.item                             as ware,
+       sum(case
+               when ch.precision < 1 then ch.qnty * ch.precision
+               else ch.qnty / 1000
+           end)                            as ware_amount
 from ch_shift cs
-left join ch_purchase cp on cp.id_shift = cs.id 
-left join ch_position ch on cp.id = ch.id_purchase 
-where 
-	cp.datecommit >= current_timestamp - interval '3 minute'
-	and cp.cashoperation = 0
-    and cp.operationtype = True
-group by 
-	cs.cashnum,
-	cs.numshift,
-	cs.shiftopen,
-	cp.numberfield,
-	ch.item
+         left join ch_purchase cp on cp.id_shift = cs.id
+         left join ch_position ch on cp.id = ch.id_purchase
+where cp.datecommit >= current_timestamp - interval '3 minute'
+  and cp.cashoperation = 0
+  and cp.operationtype = True
+group by cs.cashnum,
+         cs.numshift,
+         cs.shiftopen,
+         cp.numberfield,
+         ch.item
