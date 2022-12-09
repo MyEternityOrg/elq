ed943ec741851b77db9d15620b0dfd85a647aeed
dark <spddarkforce47@gmail.com>
2022-12-09 12:07:27 +0300

    concurent pdf templates

--- app_devices/printer/printer.py
+++ app_devices/printer/printer.py
@@ -65,6 +65,7 @@ def print_receipt(printer_name: str = 'SAM4S', receipt_id: str = '99999', receip
         print(cups.getServer())
         printers = conn.getPrinters()
         for printer in printers:
+            print(f'Cups printer: {printer}')
             if str(printer).lower() == printer_name.lower():
                 for i in range(1, receipt_count + 1):
                     conn.printFile(printer, f"{printer_name}_receipt.pdf", "document", {"page-left": "1"})
